d53e9ba37ebb7b7e2e423256143e27674775bf68
kobemhlee <kobemhlee@gmail.com>
2020-06-11 23:09:30 -0700

    Major thematic adjustments -- closer to desired end product

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Nested Tab Bar Demo',
+      title: 'Home Page',
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'Nested Tab Bar Demo Page'),
+      home: MyHomePage(title: 'Home'),
     );
   }
 }
@@ -44,53 +44,88 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        flexibleSpace: Image(
-          image: AssetImage('assets/wood.JPG'),
-          fit: BoxFit.cover,
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.home,
-              color: Colors.black87,
+    return MaterialApp(
+      home: DefaultTabController(
+          length: 3,
+          child: new Scaffold(
+            body: Stack(
+              children: <Widget>[
+                TabBarView(
+                  children: [
+                    new Container(
+                      color: Color(0xff212128),
+                    ),
+                    new Container(
+                      color: Colors.orange,
+                    ),
+                    MySongs()
+                  ],
+                  controller: _tabController,
+                ),
+                Container(
+                  height: 25,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
+                    color: Color(0xffF0BD74),
+                  ),
+                )
+              ]
+            ),
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(45.0),
+              child: AppBar(
+                elevation: 0,
+                title: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      child: Text(
+                          "ez guitar", textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 41
+                          )),
+                    )
+                ),
+                actions: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.whatshot,
+                      color: Colors.brown[800],
+                    ),
+                  )
+                ],
+                backgroundColor: Color(0xffF0BD74),
+              ),
+            ),
+            backgroundColor: Colors.white,
+            bottomNavigationBar: Material(
+              color: Color(0xffB2F2C3),
+              child: TabBar(
+                  controller: _tabController,
+                  indicatorColor: Color(0xff212128),
+                  labelColor: Color(0xff212128),
+                  unselectedLabelColor: Colors.black54,
+                  tabs: <Widget>[
+                    Tab(
+                      text: 'chord library',
+                      icon: Icon(Icons.music_note, size: 22),
+                    ),
+                    Tab(
+                      text: 'ear trainer',
+                      icon: Icon(Icons.hearing),
+                    ),
+                    Tab(
+                      text: 'my songs',
+                      icon: Icon(Icons.create),
+                    ),
+                  ]),
             ),
           )
-        ],
-      ),
-      bottomNavigationBar: Material(
-        color: Colors.black38,
-        child: TabBar(
-            controller: _tabController,
-            indicatorColor: Colors.cyan[200],
-            labelColor: Colors.cyanAccent,
-            unselectedLabelColor: Colors.black54,
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.music_note),
-              ),
-              Tab(
-                icon: Icon(Icons.hearing),
-              ),
-              Tab(
-                icon: Icon(Icons.create),
-              ),
-            ]),
-      ),
-      body: TabBarView(
-        children: <Widget>[
-          NestedTabBar(),
-
-          Center(
-            child: Text("Ear Trainer Page Goes Here"),
-          ),
-
-          MySongs()
-        ],
-        controller: _tabController,
-      ),
+        )
     );
   }
 }
